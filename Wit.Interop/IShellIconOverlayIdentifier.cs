8a27394f92532c081d8cdec7b940c667bc7fc3ed
Cody Russell <bratsche@gnome.org>
2008-12-29 15:35:01 -0600

    Overlay icons

---
+++ Wit.Interop/IShellIconOverlayIdentifier.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+
+using Microsoft.Win32;
+
+namespace Wit.Interop
+{
+    public enum IconOverlayFlags : uint
+    {
+        IconFile  = 1,
+        IconIndex = 2
+    }
+
+    [ComImport, GuidAttribute("0C6C4200-C589-11D0-999A-00C04FD655E1")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    public interface IShellIconOverlayIdentifier
+    {
+        [PreserveSig]
+        int IsMemberOf([MarshalAs(UnmanagedType.LPWStr)] string iconFileBuffer,
+                       [MarshalAs(UnmanagedType.U4)] int attributes);
+
+        [PreserveSig]
+        int GetOverlayInfo([MarshalAs(UnmanagedType.LPWStr)] out string iconFileBuffer,
+                           int iconFileBufferSize,
+                           out int iconIndex,
+                           [MarshalAs(UnmanagedType.U4)] out uint flags);
+
+        [PreserveSig]
+        int GetPriority(out int priority);
+    }
+}
---
+++ Wit.Interop/IconState.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Wit.Interop
+{
+    public enum IconState
+    {
+        Added,
+        Conflict,
+        Deleted,
+        Ignored,
+        Locked,
+        Modified,
+        Normal,
+        ReadOnly,
+        Unversioned
+    }
+}
